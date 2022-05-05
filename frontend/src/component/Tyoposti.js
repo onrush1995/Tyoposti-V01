6bdded0042227c980df102764622d89bf6f20a62
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-05 14:52:01 +0300

    Main page has been created

---
+++ frontend/src/component/Tyoposti.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssStyle from '../resources/normalizestyle /Tyopostilogo';
+
+const Tyoposti = () => {
+    return (
+        <CssStyle>
+            <h1 className='gradient-text'>Työposti</h1>
+        </CssStyle>
+    );
+};
+
+export default Tyoposti;
---
+++ frontend/src/tab/MainLanding.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tyoposti from '../component/Tyoposti';
+
+const MainLanding = () => {
+    return (
+        <main>
+            <nav>
+                <Tyoposti />
+            </nav>
+        </main>
+    );
+};
+
+export default MainLanding;
