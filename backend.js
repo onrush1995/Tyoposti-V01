d0d099dceb3747f79b8956b9c8274dfdb69fe03b
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-10 17:45:48 +0300

    Error handler created with json msg

--- backend.js
+++ backend.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import errorHandling from './tyopsotimiddleware/express-error-handler.js';
 import unknownMiddleware from './tyopsotimiddleware/unknown.js';
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
+    throw new Error('BROKEN');
     res.send('Hello World!');
 });
 
 app.use(unknownMiddleware);
+app.use(errorHandling);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
---
+++ tyopsotimiddleware/express-error-handler.js
@@ -0,0 +1,6 @@
+const errorHandling = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+};
+
+export default errorHandling;
