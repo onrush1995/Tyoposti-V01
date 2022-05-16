782174e5758617e30944727d9479038be1529e18
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-16 17:38:25 +0300

    validation error message refactor in PostMan

--- tyopsotimiddleware/express-error-handler.js
+++ tyopsotimiddleware/express-error-handler.js
@@ -1,6 +1,17 @@
+import { StatusCodes } from 'http-status-codes';
 const errorHandling = (err, req, res, next) => {
     console.log(err);
-    res.status(500).json({ msg: err });
+    const errorValue = {
+        httpStatusCodes: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: 'Please check and try again!'
+    };
+    if (err.name === 'ValidationError') {
+        errorValue.httpStatusCodes = StatusCodes.BAD_REQUEST;
+        //errorValue.msg = err.message;
+        errorValue.msg = Object.values(err.errors).map((item) => item.message).join(' | ');
+    }
+    //res.status(errorValue.httpStatusCodes).json({ msg: err });
+    res.status(errorValue.httpStatusCodes).json({ msg: errorValue.msg });
 };
 
 export default errorHandling;
