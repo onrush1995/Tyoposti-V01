d09807fdb3f5d54758f474470722e28c71c26ea5
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-17 11:43:04 +0300

    Customize code for the error handler

--- tyopostiControllers/authControllers.js
+++ tyopostiControllers/authControllers.js
@@ -4,14 +4,28 @@ import { StatusCodes } from 'http-status-codes';
 class APIErrorCode extends Error {
     constructor(message) {
         super(message);
+    }
+}
+
+class Bad_request_err_code extends APIErrorCode {
+    constructor(message) {
+        super(message);
         this.httpStatusCodes = StatusCodes.BAD_REQUEST;
     }
 }
+
+class Not_found_err_code extends APIErrorCode {
+    constructor(message) {
+        super(message);
+        this.httpStatusCodes = StatusCodes.NOT_FOUND;
+    }
+}
+
 const register = async (request, response, next) => {
     const { name, email, password } = request.body;
     // Logic for any of them is missing,it will throw error in the controller
     if (!name || !email || !password) {
-        throw new APIErrorCode('All values are required');
+        throw new Bad_request_err_code('All values are required');
     }
 
     const user = await User.create({ name, email, password });
