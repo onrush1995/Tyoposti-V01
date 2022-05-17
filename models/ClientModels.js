12c0330d6508703866b53c4ba80900aaa4dc0e16
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-17 13:36:14 +0300

    Hashing password with bcrypt

--- models/ClientModels.js
+++ models/ClientModels.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const ClientSchema = new mongoose.Schema({
     name: {
@@ -50,6 +51,16 @@ const ClientSchema = new mongoose.Schema({
         maxlength: 30,
         default: 'your city'
     }
+});
+
+// ClientSchema.pre('save', function() {
+//     console.log(this.password);
+// });
+
+// Hashing password with the help of bcrypt
+ClientSchema.pre('save', async function() {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // User will create a database in mongoDb
