9e3d275ffebe27228b313e11dae6cea6b616f56a
Moni <moniruzzaman.eng1995@gmail.com>
2022-05-18 11:49:32 +0300

    Remove password from console and setup data programmaticly

--- tyopostiControllers/authControllers.js
+++ tyopostiControllers/authControllers.js
@@ -17,7 +17,15 @@ const register = async (request, response, next) => {
     const user = await User.create({ name, email, password });
     const oAuthToken = user.createOAuth();
     // oAuthToken will print token number in the console
-    response.status(StatusCodes.CREATED).json({ user, oAuthToken });
+    response.status(StatusCodes.CREATED).json({
+        user: {
+            Name: user.name,
+            LastName: user.lastName,
+            Email: user.email,
+            Location: user.location
+        },
+        oAuthToken
+    });
 };
 
 const login = (req, res) => {
